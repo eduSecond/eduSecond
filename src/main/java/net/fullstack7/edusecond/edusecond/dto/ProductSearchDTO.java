6432c4141d9eddf79f419fb3ef9b469dfa11cadb
gyeongmin <kgmksw@naver.com>
2024-11-11 23:42:48 +0900

    Gyeongmin/0001

--- src/main/java/net/fullstack7/edusecond/edusecond/dto/ProductSearchDTO.java
+++ src/main/java/net/fullstack7/edusecond/edusecond/dto/ProductSearchDTO.java
@@ -9,28 +9,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProductSearchDTO extends CommonSearchDTO {
-    @Pattern(regexp = "^(ALL|SELLER_ID|PRODUCT_NAME|PRICE_RANGE|QUALITY)$", 
+    @Pattern(regexp = "^(ALL|SELLER_ID|PRODUCT_NAME|PRODUCT_STATUS)$", 
             message = "유효하지 않은 검색 유형입니다")
     @Override
     public void setSearchType(String searchType) {
         super.setSearchType(searchType);
     }
     
-    @Pattern(regexp = "^(NEW|LIKE_NEW|GOOD|FAIR)$")
-    private String quality;
-    
     @Pattern(regexp = "^(AVAILABLE|RESERVED|SOLD)$")
     private String productStatus;
-    
-    @Min(0)
-    private Integer minPrice;
-    
-    @Min(0)
-    private Integer maxPrice;
-    
-    @Min(0)
-    private Integer minQuantity;
-    
-    @Min(0)
-    private Integer maxQuantity;
+
 } 
