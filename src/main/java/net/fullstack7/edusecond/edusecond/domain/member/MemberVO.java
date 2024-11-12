7e30e44ee04c4cbb25d82b90e934c5d2852c7edc
gyeongmin <kgmksw@naver.com>
2024-11-12 17:40:20 +0900

    Add MemberVO.java

---
+++ src/main/java/net/fullstack7/edusecond/edusecond/domain/member/MemberVO.java
@@ -0,0 +1,30 @@
+package net.fullstack7.edusecond.edusecond.domain.member;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberVO {
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String userEmail;
+    private String userPhone;
+    private String userAddress;
+    private String userPostcode;
+    private LocalDate userBirth;
+    private LocalDateTime regDate;
+    private LocalDateTime modifyDate;
+    private String enabled;
+    
+    public boolean isEnabled() {
+        return "Y".equals(enabled);
+    }
+}
