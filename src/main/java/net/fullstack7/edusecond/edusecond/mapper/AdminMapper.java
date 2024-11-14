460c4c663b3d6e324460eb95e65e22bcf44e5bac
JerryHan <haninqq@gmail.com>
2024-11-14 14:22:15 +0900

    orderList halfway done

--- src/main/java/net/fullstack7/edusecond/edusecond/mapper/AdminMapper.java
+++ src/main/java/net/fullstack7/edusecond/edusecond/mapper/AdminMapper.java
@@ -1,7 +1,9 @@
 package net.fullstack7.edusecond.edusecond.mapper;
 
 import net.fullstack7.edusecond.edusecond.domain.AdminVO;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AdminMapper {
     AdminVO selectAdminById(String adminId);
 }
